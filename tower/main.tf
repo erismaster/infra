d4aaabef802442dbda5d1ee65455aac45339293a
Derrik Campau <dcampau1@gmail.com>
2024-08-06 20:44:48 -0700

    Add Kubernetes provider

--- tower/main.tf
+++ tower/main.tf
@@ -18,8 +18,14 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  #config_path    = "~/.kube/config"
+  config_context = "microk8s"
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
+
