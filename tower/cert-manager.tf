31d0e1c361bbda8271c233249b259439e26ba344
Derrik Campau <dcampau1@gmail.com>
2024-08-07 21:29:41 -0700

    Enable crds cert-manager

--- tower/cert-manager.tf
+++ tower/cert-manager.tf
@@ -5,9 +5,9 @@ resource "helm_release" "cert-manager" {
   
   namespace = "cert-manager"
   create_namespace = true
-  
-#   set {
-#   name  = "service.spec.loadBalancerIP"
-#   value = "10.0.1.20"
-#   }
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 }
